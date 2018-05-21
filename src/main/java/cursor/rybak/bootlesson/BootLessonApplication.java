8471977824c16864c6d244ce569f16efaaf30e3d
Nick Rybak <nrybak94@gmail.com>
2018-05-21 12:17:22 +0300

    master: get ApplicationContext

--- src/main/java/cursor/rybak/bootlesson/BootLessonApplication.java
+++ src/main/java/cursor/rybak/bootlesson/BootLessonApplication.java
@@ -2,11 +2,16 @@ package cursor.rybak.bootlesson;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class BootLessonApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BootLessonApplication.class, args);
-	}
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(BootLessonApplication.class, args);
+
+        for (String name : context.getBeanDefinitionNames()) {
+            System.out.println("================== " + name);
+        }
+    }
 }
