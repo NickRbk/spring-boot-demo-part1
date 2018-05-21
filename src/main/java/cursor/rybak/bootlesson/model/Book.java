eb4dc8863a6932e89e2e48f6edb6474e4e07c49c
Nick Rybak <nrybak94@gmail.com>
2018-05-21 11:43:30 +0300

    master: add model 'Book'

---
+++ src/main/java/cursor/rybak/bootlesson/model/Book.java
@@ -0,0 +1,14 @@
+package cursor.rybak.bootlesson.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Book {
+    private int id;
+    private String name;
+    private String description;
+}
